e8a3905884b9c68857212c3b56de3998dca19f3d
mohitkadyan <mohitkadyan1817@gmail.com>
2022-11-09 09:00:06 +0530

    exception handling

--- LoanAdminManagement/src/main/java/com/capstone/LoanAdminManagement/Controller/LoanCardController.java
+++ LoanAdminManagement/src/main/java/com/capstone/LoanAdminManagement/Controller/LoanCardController.java
@@ -1,8 +1,12 @@
 package com.capstone.LoanAdminManagement.Controller;
 
 import com.capstone.LoanAdminManagement.Model.LoanCardData;
+import com.capstone.LoanAdminManagement.Repository.LoanCardRepository;
 import com.capstone.LoanAdminManagement.Service.LoanCardService;
+import com.capstone.LoanAdminManagement.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,10 +15,21 @@ import java.util.List;
 public class LoanCardController {
     @Autowired
     LoanCardService service;
+    @Autowired
+    LoanCardRepository repo;
     @DeleteMapping("/deleteLoanCard/{lid}")
-    public String deleteLoanCard(@PathVariable("lid")int lid){
-        service.deleteLoanCard(lid);
-        return "loan card deleted";
+    public ResponseEntity<HttpStatus> deleteLoanCard(@PathVariable("lid")int lid){
+        try
+        {
+            LoanCardData loanCard = repo.findById(lid)
+                    .orElseThrow(() -> new ResourceNotFoundException("Loan Card does not exist with id: " + lid));
+            service.deleteLoanCard(lid);
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/allLoanCard")
